5d18f27410948bb8ac27eedbade2d9525ea44aba
Darya <daria.kolenchenko@gmail.com>
2025-05-01 17:21:22 +0200

    feat: add validation api

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import './App.scss'
 import type { ReactElement } from 'react'
-import { fetchProducts } from '@/server/api'
+import { authenticate, fetchProducts } from '@/server/api'
 import { useFetch } from '@/shared/hooks/useFetch'
-import type { ClientResponse } from '@commercetools/platform-sdk'
-import type { ProductPagedQueryResponse } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/product'
+import type { ClientResponse, ProductPagedQueryResponse } from '@commercetools/platform-sdk'
+import type { CustomerSignInResult } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/customer'
 
 function App(): ReactElement {
+  const {
+    data: authData,
+    error: authError,
+    isLoading: authIsLoading,
+  } = useFetch<ClientResponse<CustomerSignInResult>>(() => authenticate('example', 'example'))
   const { data, error, isLoading } = useFetch<ClientResponse<ProductPagedQueryResponse>>(fetchProducts)
   return (
     <div className="container">
-      <h1>eCommerce Application</h1>
+      <h1>eCommerce Application Login</h1>
+      {authIsLoading && <div>Loading...</div>}
+      {authError && <div>{authError.message}</div>}
+      {authData && <div>Status code: {authData.statusCode}</div>}
+
+      <h1>eCommerce Application Products</h1>
       {isLoading && <div>Loading...</div>}
       {error && <div>{error.message}</div>}
       {data && <div>Status code: {data.statusCode}</div>}
