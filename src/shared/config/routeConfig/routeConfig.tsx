504b5acdfe09dff305086cbc2f24f7865822edd9
artemgassan <artemgassan@ya.ru>
2025-05-02 22:00:54 +0300

    feat: added paths for application pages

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -2,10 +2,14 @@ import type { RouteProps } from 'react-router-dom'
 import NotFoundPage from '@/pages/NotFoundPage/NotFoundPage'
 import { MainPageAsync } from '@/pages/MainPage/MainPage.async'
 import { LoginPageAsync } from '@/pages/LoginPage/LoginPage.async'
+import { ProfilePageAsync } from '@/pages/ProfilePage/ProfilePage.async'
+import { RegisterPageAsync } from '@/pages/RegisterPage/RegisterPage.async'
 
 export enum RoutePath {
   MAIN = '/',
   LOGIN = '/login',
+  PROFILE = '/profile',
+  REGISTER = '/register',
   NOT_FOUND = '*',
 }
 
@@ -17,6 +21,14 @@ export const routeConfig: RouteProps[] = [
   {
     path: RoutePath.LOGIN,
     element: <LoginPageAsync />,
+  },
+  {
+    path: RoutePath.PROFILE,
+    element: <ProfilePageAsync />,
+  },
+  {
+    path: RoutePath.REGISTER,
+    element: <RegisterPageAsync />,
   },
   {
     path: RoutePath.NOT_FOUND,
