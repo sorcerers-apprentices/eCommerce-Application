063781e258dbd8f729eb1199b84c926df74aa963
Anna Vasilevich <dzichonka@gmail.com>
2025-05-02 14:34:32 +0200

    feat: add display f validation error for email

--- src/components/LoginForm/EmailInput.tsx
+++ src/components/LoginForm/EmailInput.tsx
@@ -1,10 +1,21 @@
 import { Label } from '@/shared/ui/Label/Label'
 import { Input } from '@/shared/ui/Input/Input'
+import { useState } from 'react'
 import type { JSX } from 'react'
 
 export const EmailInput = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState<string | null>(null)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    console.log(event.target.value)
+    const value = event.target.value
+    setEmail(value)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(value)) {
+      setError('Please enter a valid email')
+    } else {
+      setError(null)
+    }
+    console.log(email)
   }
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
     if (event.key === ' ') {
@@ -24,9 +35,10 @@ export const EmailInput = (): JSX.Element => {
         onKeyDown={handleKeyDown}
         onChange={handleChange}
         required
-        className={['form__input']}
+        className={['form__input', error ? 'input--error' : '']}
         placeholder="Avada"
       />
+      {error && <span className="form__error">{error}</span>}
     </div>
   )
 }
