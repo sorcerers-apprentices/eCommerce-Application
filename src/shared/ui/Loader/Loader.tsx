99ae222adb7888efea8df77dd62f03f501711640
Anna Vasilevich <dzichonka@gmail.com>
2025-05-07 23:20:18 +0200

    feat: add dots for loder using map

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -2,20 +2,12 @@ import s from './Loader.module.scss'
 import type { ReactElement } from 'react'
 
 const Loader = (): ReactElement => {
+  const NUMBER_OF_DOTS = 12
   return (
     <div className={s.lds}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      {Array.from({ length: NUMBER_OF_DOTS }).map((_, index) => (
+        <div key={index}></div>
+      ))}
     </div>
   )
 }
