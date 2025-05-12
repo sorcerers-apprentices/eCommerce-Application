266661881d0597bb49805a20f9965086783c5dd7
artemgassan <artemgassan@ya.ru>
2025-05-12 23:13:36 +0300

    feat: implemented redirect for login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,19 @@
-import type { ReactElement } from 'react'
+import { useUser } from '@/hooks/useUser'
+import { useNavigate } from 'react-router-dom'
+import { type ReactElement, useEffect } from 'react'
 import { LoginForm } from '@/components/LoginForm/LoginForm'
+import { RoutePath } from '@/shared/config/routeConfig/routeConfig'
 
 const LoginPage = (): ReactElement => {
+  const { state } = useUser()
+  const navigation = useNavigate()
+
+  useEffect((): void => {
+    if (state.isAuth) {
+      navigation(RoutePath.MAIN)
+    }
+  }, [state.isAuth, navigation])
+
   return (
     <div>
       <LoginForm />
