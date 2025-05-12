2f94c80a716ea3e9e187ef1a92fda372e0f72528
Darya <daria.kolenchenko@gmail.com>
2025-05-12 23:09:39 +0200

    feat: add logout function to eCommers tool and update useAuth

--- src/server/auth-api.ts
+++ src/server/auth-api.ts
@@ -3,12 +3,15 @@ import {
   createPasswordRequestBuilder,
   createRefreshBuilder,
   createRegistrationRequestBuilder,
+  tokenCache,
+  tokenCacheKey,
 } from '@/server/client'
 import type { ClientResponse } from '@commercetools/platform-sdk'
 import type { CustomerSignInResult } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/customer'
 import type { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder'
+import { environment } from '@/app/types/environment.ts'
 
-const anonymousBuilder: ByProjectKeyRequestBuilder = createAnonymousRequestBuilder()
+let anonymousBuilder: ByProjectKeyRequestBuilder = createAnonymousRequestBuilder()
 let refreshBuilder: ByProjectKeyRequestBuilder | undefined
 
 export const builder = (): ByProjectKeyRequestBuilder => refreshBuilder || anonymousBuilder
@@ -29,7 +32,8 @@ export const authApi = {
   register: async (parameters: RegistrationParameters): Promise<ClientResponse<CustomerSignInResult>> => {
     const registrationBuilder = createRegistrationRequestBuilder()
     await registrationBuilder
-      .customers()
+      .me()
+      .signup()
       .post({
         body: {
           email: parameters.email,
@@ -52,7 +56,7 @@ export const authApi = {
   },
   authenticate: async (email: string, password: string): Promise<ClientResponse<CustomerSignInResult>> => {
     const passwordBuilder = createPasswordRequestBuilder(email, password)
-    const authenticationResult = passwordBuilder
+    const authenticationResult = await passwordBuilder
       .me()
       .login()
       .post({
@@ -61,5 +65,23 @@ export const authApi = {
       .execute()
     refreshBuilder = createRefreshBuilder()
     return authenticationResult
+  },
+  logout: async (): Promise<void> => {
+    const refreshToken = tokenCache.get().refreshToken
+    if (refreshToken) {
+      await fetch(`${environment.AUTH_URL}/oauth/token/revoke`, {
+        method: 'POST',
+        headers: {
+          Authorization: 'Basic ' + btoa(`${environment.CLIENT_ID}:${environment.CLIENT_SECRET}`),
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          token: refreshToken,
+        }),
+      })
+    }
+    refreshBuilder = undefined
+    localStorage.removeItem(tokenCacheKey)
+    anonymousBuilder = createAnonymousRequestBuilder()
   },
 }
