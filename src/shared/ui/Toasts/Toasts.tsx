1f584384e6f4fbbf7631e4632106e6176054c93b
artemgassan <artemgassan@ya.ru>
2025-05-12 12:54:38 +0300

    feat: add styles for notification

--- src/shared/ui/Toasts/Toasts.tsx
+++ src/shared/ui/Toasts/Toasts.tsx
@@ -1,14 +1,16 @@
 import s from './Toasts.module.scss'
+import { IoCheckmarkCircle } from 'react-icons/io5'
 import { type ReactElement, useEffect, useState } from 'react'
+import { BiSolidError, BiSolidErrorCircle } from 'react-icons/bi'
 
 type ToastProperties = {
   type: 'error' | 'info' | 'warning' | 'success'
   message: string
 }
 
-const toastDisplayTime = 3000
+const toastDisplayTime = 100000
 
-const Toast = (parameter: ToastProperties): ReactElement => {
+const Toast = (parameter: ToastProperties): ReactElement | null => {
   const [isVisible, setIsVisible] = useState(true)
 
   useEffect(() => {
@@ -23,11 +25,27 @@ const Toast = (parameter: ToastProperties): ReactElement => {
     return `${s.toast} ${s[parameter.type]}`
   }
 
-  if (!isVisible) return <></>
+  const getToastIcon = (): ReactElement | null => {
+    switch (parameter.type) {
+      case 'success':
+        return <IoCheckmarkCircle color="white" size={20} />
+      case 'warning':
+        return <BiSolidError color="white" size={20} />
+      case 'error':
+        return <BiSolidError color="white" size={20} />
+      case 'info':
+        return <BiSolidErrorCircle color="white" size={20} />
+      default:
+        return null
+    }
+  }
+
+  if (!isVisible) return null
 
   return (
     <div className={getToastClassName()}>
-      <p>{parameter.message}</p>
+      {getToastIcon()}
+      <p className={s.text}>{parameter.message}</p>
     </div>
   )
 }
