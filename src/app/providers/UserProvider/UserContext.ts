3d304ed9c411920e4480333b8489fdf4b47c0691
artemgassan <artemgassan@ya.ru>
2025-05-24 22:12:03 +0300

    refactor: actions have been moved

--- src/app/providers/UserProvider/UserContext.ts
+++ src/app/providers/UserProvider/UserContext.ts
@@ -1,40 +1,13 @@
+import type { UserAction } from './UserReducer'
 import { createContext, type Dispatch } from 'react'
-
-export type UserContext = {
-  state: UserState
-  dispatch: Dispatch<UserAction>
-}
 
 export type UserState = {
   email?: string
 }
 
-export type UserAction = SetUser | UpdateUser | LoginUser | LogoutUser
-
-export enum UserActionType {
-  SET_ALL = 'SET_ALL',
-  UPDATE = 'UPDATE',
-  LOGIN = 'LOGIN',
-  LOGOUT = 'LOGOUT',
-}
-
-type SetUser = {
-  type: UserActionType.SET_ALL
-  payload: UserState
-}
-
-type UpdateUser = {
-  type: UserActionType.UPDATE
-  payload: Partial<UserState>
-}
-
-type LoginUser = {
-  type: UserActionType.LOGIN
-  payload: { email: string }
-}
-
-type LogoutUser = {
-  type: UserActionType.LOGOUT
+export type UserContext = {
+  state: UserState
+  dispatch: Dispatch<UserAction>
 }
 
 export const userContext = createContext<UserContext>({
--- src/app/providers/UserProvider/UserProvider.tsx
+++ src/app/providers/UserProvider/UserProvider.tsx
@@ -1,12 +1,12 @@
-import { type JSX, type ProviderProps, useReducer } from 'react'
-import { UserActionType, userContext, type UserState } from './UserContext'
-import { useFetch } from '@/shared/hooks/useFetch.tsx'
-import type { ClientResponse, Customer } from '@commercetools/platform-sdk'
-import { api } from '@/server/api.ts'
-import { isAnonymous } from '@/server/client.ts'
-import Loader from '@/shared/ui/Loader/Loader.tsx'
-import { userReducer } from '@/app/providers/UserProvider/UserReducer.ts'
+import { api } from '@/server/api'
 import { toast } from 'react-hot-toast'
+import { isAnonymous } from '@/server/client'
+import Loader from '@/shared/ui/Loader/Loader'
+import { useFetch } from '@/shared/hooks/useFetch'
+import { userContext, type UserState } from './UserContext'
+import { UserActionType, userReducer } from './UserReducer'
+import { type JSX, type ProviderProps, useReducer } from 'react'
+import type { ClientResponse, Customer } from '@commercetools/platform-sdk'
 
 export const UserProvider = ({ value, children }: Partial<ProviderProps<UserState>>): JSX.Element => {
   const [state, dispatch] = useReducer(userReducer, value || {})
