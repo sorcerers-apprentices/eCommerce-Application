06de4a2086d2809c1838f29eaa0ecdb4b3198f1a
artemgassan <artemgassan@ya.ru>
2025-05-25 19:09:16 +0300

    fix: fixed endless queries

--- src/app/providers/UserProvider/UserProvider.tsx
+++ src/app/providers/UserProvider/UserProvider.tsx
@@ -4,24 +4,37 @@ import { isAnonymous } from '@/server/client'
 import { useFetch } from '@/shared/hooks/useFetch'
 import { userContext, type UserState } from './UserContext'
 import { UserActionType, userReducer } from './UserReducer'
-import { type JSX, type ProviderProps, useReducer } from 'react'
 import type { ClientResponse, Customer } from '@commercetools/platform-sdk'
+import { type JSX, type ProviderProps, useCallback, useMemo, useReducer } from 'react'
 
 export const UserProvider = ({ value, children }: Partial<ProviderProps<UserState>>): JSX.Element => {
   const [state, dispatch] = useReducer(userReducer, value || {})
-  const { loading } = useFetch<ClientResponse<Customer>>(api.user.fetchMe, {
-    enabled: !isAnonymous(),
-    onSuccess: (data) => {
-      const email = data?.body.email
+
+  const fetchSuccess = useCallback(
+    (data: ClientResponse<Customer>) => {
+      const email = data.body.email
       if (email) {
         dispatch({ type: UserActionType.UPDATE, payload: { email } })
       }
     },
-    onFailure: () => {
-      toast.error('Authorization failed')
-      dispatch({ type: UserActionType.UPDATE, payload: { email: undefined } })
-    },
-  })
+    [dispatch]
+  )
+
+  const fetchFailure = useCallback(() => {
+    toast.error('Authorization failed')
+    dispatch({ type: UserActionType.UPDATE, payload: { email: undefined } })
+  }, [dispatch])
+
+  const fetchOptions = useMemo(
+    () => ({
+      enabled: !isAnonymous(),
+      onSuccess: fetchSuccess,
+      onFailure: fetchFailure,
+    }),
+    [fetchSuccess, fetchFailure]
+  )
+
+  const { loading } = useFetch<ClientResponse<Customer>>(api.user.fetchMe, fetchOptions)
 
   return <userContext.Provider value={{ state, dispatch, loading }}>{children}</userContext.Provider>
 }
