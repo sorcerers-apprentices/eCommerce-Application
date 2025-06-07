aa472f459982f61ec57c339d004964f869b20de7
artemgassan <artemgassan@ya.ru>
2025-06-07 20:30:15 +0300

    refactor: changed the component's credentials

--- src/components/Header/UserBar/UserBar.tsx
+++ src/components/Header/UserBar/UserBar.tsx
@@ -4,10 +4,14 @@ import type { ReactElement } from 'react'
 import { MdFavoriteBorder } from 'react-icons/md'
 import { UserButton } from '../UserButton/UserButton'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
-import CartCount from '@/components/Header/CartCount/CartCount'
+import { useCartContext } from '@/hooks/useCartContext'
+import CartCounter from '@/components/Header/CartCount/CartCounter'
 import { RoutePath } from '@/shared/config/routeConfig/routeConfig'
 
 export const UserBar = (): ReactElement => {
+  const { state } = useCartContext()
+  const countProducts = state.countProducts
+
   return (
     <nav className={s.navbar}>
       <NavLink to={RoutePath.MAIN} className={s.icon}>
@@ -15,7 +19,7 @@ export const UserBar = (): ReactElement => {
       </NavLink>
       <NavLink to={RoutePath.CART} className={s.icon}>
         <HiOutlineShoppingCart className="icon" />
-        <CartCount />
+        <CartCounter count={countProducts} />
       </NavLink>
       <UserButton />
     </nav>
