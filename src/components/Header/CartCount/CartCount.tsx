99abd9e65bc11b84daafce7f934ca1daec9221bc
artemgassan <artemgassan@ya.ru>
2025-06-07 19:51:33 +0300

    feat: the bucket counter is connected to the provider

--- src/components/Header/CartCount/CartCount.tsx
+++ src/components/Header/CartCount/CartCount.tsx
@@ -1,10 +1,15 @@
 import s from './CartCount.module.scss'
 import type { ReactElement } from 'react'
+import { useCartContext } from '@/hooks/useCartContext'
 
-const CartCount = (): ReactElement => {
+const CartCount = (): ReactElement | void => {
+  const { state } = useCartContext()
+  if (!state.countProducts || state.countProducts < 1) return
+  const countProducts = String(state.countProducts)
+
   return (
     <div className={s.cartcount}>
-      <p className={s.count}>4</p>
+      <p className={s.count}>{countProducts}</p>
     </div>
   )
 }
--- src/hooks/useCartContext.tsx
+++ src/hooks/useCartContext.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { CartContext } from '@/app/providers/CartProvider/CartContext'
 
-export const useCart = (): CartContext => {
+export const useCartContext = (): CartContext => {
   const basket: CartContext = useContext(CartContext)
   if (!basket) {
     throw new Error('Missing Cart Context')
