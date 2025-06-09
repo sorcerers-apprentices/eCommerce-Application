e2d75e45ca7ee200b7b15733d592bac4a0044c57
Anna Vasilevich <dzichonka@gmail.com>
2025-06-09 08:14:50 +0200

    feat: add message and link for empty cart

--- src/components/CartSection/CartTable/CartTable.tsx
+++ src/components/CartSection/CartTable/CartTable.tsx
@@ -7,44 +7,60 @@ import type { Cart, ClientResponse } from '@commercetools/platform-sdk'
 import Loader from '@/shared/ui/Loader/Loader'
 import { CartMapper } from '../CartMapper'
 import { useCart } from '@/hooks/useCart'
+import s from '../CartSection.module.scss'
+import { Button } from '@/shared/ui/Button/Button'
+import { NavLink } from 'react-router-dom'
 
 export const CartTable = (): JSX.Element => {
   const { data, error, loading, refetch } = useFetch<ClientResponse<Cart>>(api.cart.fetchActiveCart)
   const { clearCart } = useCart()
   return (
-    <>
+    <div className="section">
       {loading && <Loader />}
       {error && <div>{error.message}</div>}
-      {data && (
-        <table>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-              <th>
-                <button
-                  type="button"
-                  onClick={async () => {
-                    if (data?.body.id) {
-                      await clearCart()
-                      refetch()
-                    }
-                  }}
-                >
-                  Clear all
-                </button>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {CartMapper.toCartView(data.body).map((product) => (
-              <CartRow key={product.id} cartItemData={product} productLink={RoutePath.MAIN} refetch={refetch} />
-            ))}
-          </tbody>
-        </table>
-      )}
-    </>
+
+      {data &&
+        (data?.body.lineItems.length ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>
+                  <button
+                    type="button"
+                    onClick={async () => {
+                      if (data?.body.id) {
+                        await clearCart()
+                        refetch()
+                      }
+                    }}
+                  >
+                    Clear all
+                  </button>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {CartMapper.toCartView(data.body).map((product) => (
+                <CartRow key={product.id} cartItemData={product} productLink={RoutePath.MAIN} refetch={refetch} />
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className={s.empty}>
+            <div>
+              Your cart is empty.
+              <br />
+              Go away… and don’t come back without some goodies!
+            </div>
+            <NavLink to={RoutePath.CATALOG}>
+              <Button>Go to catalog</Button>
+            </NavLink>
+          </div>
+        ))}
+    </div>
   )
 }
