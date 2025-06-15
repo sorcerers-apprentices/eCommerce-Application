f3fa02b6d0a33c29491be0b89f6cf074d05895d2
Anna Vasilevich <dzichonka@gmail.com>
2025-06-15 08:29:00 +0200

    feat: add promo codes to main page

--- src/components/PromoSection/PromoSection.tsx
+++ src/components/PromoSection/PromoSection.tsx
@@ -10,7 +10,10 @@ export const PromoSection = (): ReactElement => {
           <h2>For all pet lovers and magic believers!</h2>
           <p>use promo code</p>
           <h2 className="title">Reviewer20</h2>
-          <p>and enjoy 20% discount!</p>
+          <p>and enjoy 20% discount for all goods</p>
+          <p>and</p>
+          <h2 className={`title ${s.title}`}>Dog-food-10</h2>
+          <p>10% discount for dog food</p>
         </div>
       </div>
     </section>
