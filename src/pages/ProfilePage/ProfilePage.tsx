3c97ed08a359bc04d8e5d18459343024913b796e
artemgassan <artemgassan@ya.ru>
2025-06-16 22:10:48 +0300

    feat: added footer

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/Header/Header'
 import { ProfileSection } from '@/components/Profile/ProfileSection/ProfileSection'
 import type { ReactElement } from 'react'
+import Footer from '@/components/Footer/Footer.tsx'
 
 const ProfilePage = (): ReactElement => {
   return (
     <>
       <Header />
       <ProfileSection />
+      <Footer />
     </>
   )
 }
