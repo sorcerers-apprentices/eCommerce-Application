109f362a768fad22ebbb382429aaaa2fafe2a511
Darya <daria.kolenchenko@gmail.com>
2025-06-16 20:49:38 +0200

    fix: delete heart icon

--- src/components/Header/UserBar/UserBar.tsx
+++ src/components/Header/UserBar/UserBar.tsx
@@ -1,7 +1,6 @@
 import s from './UserBar.module.scss'
 import { NavLink } from 'react-router'
 import type { ReactElement } from 'react'
-import { MdFavoriteBorder } from 'react-icons/md'
 import { UserButton } from '../UserButton/UserButton'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
 import { useCartContext } from '@/hooks/useCartContext'
@@ -14,9 +13,6 @@ export const UserBar = (): ReactElement => {
 
   return (
     <nav className={s.navbar}>
-      <NavLink to={RoutePath.MAIN} className={s.icon}>
-        <MdFavoriteBorder className="icon" />
-      </NavLink>
       <NavLink to={RoutePath.CART} className={s.icon}>
         <HiOutlineShoppingCart className="icon" />
         <CartCounter count={countProducts} />
