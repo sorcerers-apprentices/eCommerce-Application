07d51f5fca1e3e9cee2cc6dd2a0e08d2ed6d0416
Anna Vasilevich <dzichonka@gmail.com>
2025-06-21 17:08:48 +0200

    fix: fix test for empty cart

--- src/components/CartSection/CartTable/CartTable.test.tsx
+++ src/components/CartSection/CartTable/CartTable.test.tsx
@@ -1,10 +1,3 @@
-import { describe, it, expect, vi } from 'vitest'
-import { CartTable } from './CartTable'
-import { render, screen } from '@testing-library/react'
-import type { Cart, ClientResponse } from '@commercetools/platform-sdk'
-import { MemoryRouter } from 'react-router-dom'
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig'
-
 const mockData = {
   body: {
     lineItems: [],
@@ -29,7 +22,12 @@ vi.mock('@/shared/hooks/useFetch', () => ({
     refetch: vi.fn(),
   }),
 }))
-
+import { describe, it, expect, vi } from 'vitest'
+import { CartTable } from './CartTable'
+import { render, screen } from '@testing-library/react'
+import type { Cart, ClientResponse } from '@commercetools/platform-sdk'
+import { MemoryRouter } from 'react-router-dom'
+import { RoutePath } from '@/shared/config/routeConfig/routeConfig'
 describe('CartTable component', () => {
   it('renders empty cart message and "Go to catalog" button', () => {
     render(
